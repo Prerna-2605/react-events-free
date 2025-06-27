3d5c0507f4a195d7a4e27edf7c4f7b30554eed22
Prerna-2605 <prernakukreja05@gmail.com>
2025-06-27 11:23:16 +0530

    made folders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './App.css'
+import Navbar from './components/common/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+// import logo from '../assets/svg/logo.svg'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='max-w-[1320px] mx-auto '>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
